b2a0a88ac7f122805097e147a27494151fcef65f
Ho <rbish005@BKUSL-1FP6P3XY.local>
2020-08-15 16:31:17 -0400

    send click info to API

---
+++ public/script.js
@@ -0,0 +1,35 @@
+
+if (!localStorage.getItem('userId')) {
+  localStorage.setItem('userId', String(Math.random()));
+}
+
+
+const handleClick = event => {
+  console.log(event)
+}
+
+const body = event => {
+  const pageX = Math.round(event.pageX);
+  const pageY = Math.round(event.pageY);
+  const findFirstDataId = event.path.find(item => item.dataset.trackingid !== undefined);
+  const theItemThatGotClicked = event.Target;
+  const howLongBeforetheClick = Math.round(event.timestamp);
+  const userId = localStorage.getItem('userId');
+
+  const url = '/clicks';
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      user: userId,
+      
+    }),
+  });
+  await tallyVotes();
+}
+
+
+window.addEventListener('click', handleClick)
